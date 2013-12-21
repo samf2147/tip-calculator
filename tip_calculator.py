ba2a9488a72c974cb8993046303a181a2541a0bc
Sam Frank <sfrank2147@gmail.com>
2013-12-21 11:01:18 -0500

    uses optparse instead of argv

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,12 +1,23 @@
-import sys
+from optparse import OptionParser
 
-if(len(sys.argv) != 4):
-	print 'Usage: python tip_calculator.py meal tax tip'
-	quit()
+parser = OptionParser()
 
-meal = float(sys.argv[1])
-tax = float(sys.argv[2])
-tip = float(sys.argv[3])
+parser.add_option('-m','--meal',dest = 'meal', 
+	help = 'the cost of the meal in dollars')
+parser.add_option('-x','--tax',dest = 'tax',
+	help = 'the tax rate')
+parser.add_option('-t','--tip',dest = 'tip',
+	help = 'the tip rate', default = '15.0')
+	
+(options,args) = parser.parse_args()
+
+if not (options.meal and options.tax):
+	parser.error('Need an argument for -m and -x')
+
+
+meal = float(options.meal)
+tax = float(options.tax)
+tip = float(options.tip)
 
 tax_value = 0.01 * tax * meal
 meal_with_tax = meal + tax_value
@@ -16,6 +27,6 @@ total = meal_with_tax + tip_value
 print 'The base cost of your meal was $%.2f' % meal
 print 'You need to pay $%.2f for tax' % tax_value
 print 'Tipping at a rate of %.2f%%, you should leave $%.2f for a tip' \
-	% (tax, tip_value)
+	% (tip, tip_value)
 print 'The grand total of your meal is $%.2f' \
 	% (total)
